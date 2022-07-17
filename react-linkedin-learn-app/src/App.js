62dc22599c5da1dc5e23276d98762c18af619230
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-17 20:09:57 +0300

    feat: use dependency array

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -3,12 +3,18 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState('happy');
-  useEffect(() => console.log(`Emotion is ${emotion}`), [emotion]);
+  const [secondEmo, setSecondEmo] = useState('tired');
+  useEffect(() => console.log(`Emotion is ${emotion}`), [emotion, secondEmo]);
+  useEffect(() => console.log(`Sec emo is ${secondEmo}`), [secondEmo]);
+
   return (
     <div className='App'>
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion('sad')}>Make me sad</button>
       <button onClick={() => setEmotion('excited')}>Make me excited</button>
+      <h2>Secondary emotion is {secondEmo}</h2>
+      <button onClick={() => setSecondEmo('grateful')}>Make me grateful</button>
+      <button onClick={() => setSecondEmo('tired')}>Make me tired</button>
     </div>
   );
 }
