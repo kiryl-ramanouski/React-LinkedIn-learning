f856596cad799b214f59168b95cade3d1cf51ca9
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-17 20:49:29 +0300

    feat: use useReducer hook

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState('happy');
-  const [secondEmo, setSecondEmo] = useState('tired');
-  useEffect(() => console.log(`Emotion is ${emotion}`), [emotion, secondEmo]);
-  useEffect(() => console.log(`Sec emo is ${secondEmo}`), [secondEmo]);
-
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className='App'>
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion('sad')}>Make me sad</button>
-      <button onClick={() => setEmotion('excited')}>Make me excited</button>
-      <h2>Secondary emotion is {secondEmo}</h2>
-      <button onClick={() => setSecondEmo('grateful')}>Make me grateful</button>
-      <button onClick={() => setSecondEmo('tired')}>Make me tired</button>
+      <input type='checkbox' value={checked} onChange={setChecked} />
+      <label>{checked ? 'Checked' : 'Not checked'}</label>
     </div>
   );
 }
