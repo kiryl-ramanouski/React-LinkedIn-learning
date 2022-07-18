a2649d50aaa68a6ab6a127591fcb1db77a19614b
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-18 15:04:15 +0300

    feat: use useState and useEffect with fetch

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -1,38 +1,17 @@
 import './App.css';
-import { useState } from 'react';
-
-// I can also use libraries for forms, such as
-// formik.org;
-// react-hook-form.com
-// And there are a lot of useful hooks useHooks.com
-
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value),
-    },
-    () => setValue(initialValue),
-  ];
-}
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [titleProps, resetTitle] = useInput('');
-  const [colorProps, resetColor] = useInput('#999999');
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value} ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type='text' placeholder='color title...' />
-      <input {...colorProps} type='color' />
-      <button>Add</button>
-    </form>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('https://api.github.com/users/whitekvazar').then((response) =>
+      response.json().then(setData)
+    );
+  }, []);
+  if (data) {
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  }
+  return <h1>No data</h1>;
 }
 
 export default App;
