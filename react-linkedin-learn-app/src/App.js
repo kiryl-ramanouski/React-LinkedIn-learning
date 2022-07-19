d874ff9652e745ab59bbef6ad4cfb572380e05f4
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-19 13:16:11 +0300

    feat: install react router and prepare the app

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -1,36 +1,19 @@
 import './App.css';
 
-const tahoePeaks = [
-  { name: 'Freel', elevation: 10891 },
-  { name: 'Monument', elevation: 10067 },
-  { name: 'Pyramid', elevation: 9983 },
-  { name: 'Tallac', elevation: 9735 },
-];
+function Home() {
+  return <h1>Main page</h1>;
+}
 
-function List({ data, renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>{renderItem(item)}</li>
-      ))}
-    </ul>
-  );
+function About() {
+  return <h1>About Us</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Us</h1>;
 }
 
 function App() {
-  return (
-    <List
-      data={tahoePeaks}
-      renderEmpty={<p>This list is empty.</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      )}
-    />
-  );
+  return <Home />;
 }
 
 export default App;
