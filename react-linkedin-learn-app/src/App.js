d1a801cc65a3742500c42532c977c253ebe19e38
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-19 13:46:42 +0300

    feat: use react router

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -4,16 +4,14 @@ function Home() {
   return <h1>Main page</h1>;
 }
 
-function About() {
+export function About() {
   return <h1>About Us</h1>;
 }
 
-function Contact() {
+export function Contact() {
   return <h1>Contact Us</h1>;
 }
 
-function App() {
+export function App() {
   return <Home />;
 }
-
-export default App;
