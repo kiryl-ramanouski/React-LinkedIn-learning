5d4768db992498b9349360c153c81c9f58832800
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-19 14:47:12 +0300

    feat: use links from react router dom

--- react-linkedin-learn-app/src/App.js
+++ react-linkedin-learn-app/src/App.js
@@ -1,15 +1,40 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function Home() {
-  return <h1>Main page</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+      <h1>Main page</h1>
+    </div>
+  );
 }
 
 export function About() {
-  return <h1>About Us</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+      <h1>About Us</h1>
+    </div>
+  );
 }
 
 export function Contact() {
-  return <h1>Contact Us</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+      <h1>Contact Us</h1>
+    </div>
+  );
 }
 
 export function App() {
