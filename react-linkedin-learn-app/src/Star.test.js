04a9640691fbad6b7b6fbfe5408404aad9eb9cb6
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-20 09:46:59 +0300

    feat: add additional-old variant of test

--- react-linkedin-learn-app/src/Star.test.js
+++ react-linkedin-learn-app/src/Star.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { Star } from './Star';
 
-test('renders an h1', () => {
+test('renders Cool Star text', () => {
   render(<Star />);
   expect(screen.getByText('Cool Star')).toBeInTheDocument();
+});
+
+test('renders an h1', () => {
+  const { getByText } = render(<Star />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const h1 = getByText(/Cool Star/);
+  expect(h1).toHaveTextContent('Cool Star');
 });
