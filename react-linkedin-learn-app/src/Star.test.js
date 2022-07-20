86af7bb4d36e2b9697374f71f886231608f3a86b
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-20 10:51:25 +0300

    refactor: star.test.js file

--- react-linkedin-learn-app/src/Star.test.js
+++ react-linkedin-learn-app/src/Star.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { Star } from './Star';
 
-test('renders Cool Star text', () => {
-  render(<Star />);
-  expect(screen.getByText('Cool Star')).toBeInTheDocument();
-});
-
+// old test version
 test('renders an h1', () => {
   const { getByText } = render(<Star />);
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const h1 = getByText(/Cool Star/);
   expect(h1).toHaveTextContent('Cool Star');
+});
+
+// modern test version
+test('renders Cool Star text', () => {
+  render(<Star />);
+  expect(screen.getByText('Cool Star')).toBeInTheDocument();
 });
