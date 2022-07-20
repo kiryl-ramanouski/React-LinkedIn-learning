3bb660af3d59984633fa2c042397da608abe8ea8
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-20 10:50:30 +0300

    feat: add modern test version with screen

--- react-linkedin-learn-app/src/Checkbox.test.js
+++ react-linkedin-learn-app/src/Checkbox.test.js
@@ -1,10 +1,19 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { Checkbox } from './Checkbox';
 
+// old test version
 test('selecting checkbox should change value to true', () => {
   const { getByLabelText } = render(<Checkbox />);
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const checkbox = getByLabelText(/not checked/);
+  fireEvent.click(checkbox);
+  expect(checkbox.checked).toEqual(true);
+});
+
+// modern test version
+test('selecting checkbox should change value to true (modern test version)', () => {
+  render(<Checkbox />);
+  const checkbox = screen.getByLabelText(/not checked/);
   fireEvent.click(checkbox);
   expect(checkbox.checked).toEqual(true);
 });
